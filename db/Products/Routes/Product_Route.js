5ec00d723d61c6955992ec1436ff36387c4960a1
Victor Okechukwu <vicoke360@gmail.com>
2023-07-08 05:21:23 +0100

    Updates made

--- db/Products/Routes/Product_Route.js
+++ db/Products/Routes/Product_Route.js
@@ -2,8 +2,29 @@ const express = require("express");
 const route = express.Router();
 
 
-const {HandleNewProduct} = require("../Product_Control/Product_Controllers")
+const {
+    HandleNewProduct,
+    HandleGetProducts,
+    HandleGetProduct,
+    HandleDeleteProduct,
+    HandleupdateProduct
+} = require("../Product_Control/Product_Controllers")
 
 route.post('/addproduct', HandleNewProduct)
+route.get('/products', HandleGetProducts)
+route.delete("/product/:id", HandleDeleteProduct),
+
+route.get("/product/:id", HandleGetProduct)
+
+route.put("/product/:id", HandleupdateProduct);
+
+// route.put("/product/:id", async (req, resp) => {
+//     let result = await Product.updateOne(
+//         { _id: req.params.id },
+//         { $set: req.body }
+//     )
+//     resp.send(result)
+// });
+
 
 module.exports = route
